4f3662b9cf4790e519dbc843928588935adf4f50
Facu Grasso <facundofggrasso@gmail.com>
2018-09-27 20:17:30 -0300

    added about section

---
+++ front-end/js/my-js/about.js
@@ -0,0 +1,9 @@
+function showLicense() {
+    document.getElementById("licenceText").classList.remove("hidden");
+    document.getElementById("licenceText").classList.add("visible");
+}
+
+function hideLicense() {
+    document.getElementById("licenceText").classList.add("hidden");
+    document.getElementById("licenceText").classList.remove("visible");
+}
